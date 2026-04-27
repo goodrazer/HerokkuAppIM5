10a378587baf581532f883d91103994a8b13f465
Malevaniy Anton <goodrazer111@gmail.com>
2026-04-27 19:50:43 +0400

    HomeWork: HerokkuApp_SeleniumAdvansed - removed redundant options.

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -21,14 +21,6 @@ public class BaseTest {
     public void setUp () {
         ChromeOptions options = new ChromeOptions();
         HashMap<String, Object> chromePrefs = new HashMap<>();
-        chromePrefs.put("credentials_enable_service", false);
-        chromePrefs.put("profile.password_manager_enabled", false);
-        chromePrefs.put("profile.password_manager_leak_detection", false);
-        options.addArguments("--start-maximized");
-        options.addArguments("--incognito");
-        options.addArguments("--disable-notfications");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-infobars");
         driver = new ChromeDriver(options);
         contextMenuPage = new ContextMenuPage(driver);
         dynamicControlsPage = new DynamicControlsPage(driver);
